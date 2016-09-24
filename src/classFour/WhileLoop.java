0ce7a7ed6cc47b5526dbfd148494616054abbf03
Yan Ren <ryan1219@live.com>
2016-09-24 13:21:17 -0400

    class four: "loop"

---
+++ src/classFour/WhileLoop.java
@@ -0,0 +1,15 @@
+package classFour;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		int i = 0;
+		
+		while(i<10){
+			System.out.println("i value is: " + i);
+			i++;
+		}
+		
+	}
+
+}
--- src/classThree/Qb.java
+++ src/classThree/Qb.java
@@ -4,9 +4,9 @@ public class Qb {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n = 0;
+		long n = 0;
 		for(; n<Integer.MAX_VALUE; n++){
-			if(((int)Math.pow(4, n)+ (int)Math.pow(7, n) + (int)Math.pow(20, n)) % 31 == 0){
+			if(((long)Math.pow(4, n)+ (long)Math.pow(7, n) + (long)Math.pow(20, n)) % 31 == 0){
 				System.out.println(n);
 			}
 		}
