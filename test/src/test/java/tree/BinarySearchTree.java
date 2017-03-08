00b5f0b4f74d4c596ed4d7894fa83a7fa26edcd9
Yan Ren <ryan.ren3@gmail.com>
2017-03-07 19:54:32 -0500

    BST continue

--- test/src/test/java/tree/BinarySearchTree.java
+++ test/src/test/java/tree/BinarySearchTree.java
@@ -5,7 +5,7 @@ public class BinarySearchTree {
 	 * BST - nodes smaller than root goes to the left and greater goes to the
 	 * right
 	 */
-	public static Node root;
+	public Node root;
 
 	public BinarySearchTree() {
 		this.root = null;
@@ -38,6 +38,79 @@ public class BinarySearchTree {
 				return;
 			}
 		}
+	}
+	
+	public Node getMinInTree(){
+		return minNode(this.root);
+	}
+	
+	private Node minNode(Node start){
+		if(start.left == null){
+			return start;
+		}else{
+			return minNode(start.left);
+		}
+	}
+	
+	public boolean deleteInTree(int id){
+		return delete(this.root, id);
+	}
+	
+	private boolean delete(Node startNode, int id){
+		Node parent = startNode;
+		Node current = startNode;
+		boolean isLeftChild = false;
+		while(current.data!=id){
+			parent = current;
+			if(current.data>id){
+				isLeftChild = true;
+				current = current.left;
+			}else{
+				isLeftChild = false;
+				current = current.right;
+			}
+			if(current ==null){
+				return false;
+			}
+		}
+		//if i am here that means we have found the node
+		//Case 1: if node to be deleted has no children
+		if(current.left==null && current.right==null){
+			if(current==startNode){
+				startNode = null;
+			}
+			if(isLeftChild ==true){
+				parent.left = null;
+			}else{
+				parent.right = null;
+			}
+		}
+		//Case 2 : if node to be deleted has only one child
+		else if(current.right==null){
+			if(current==startNode){
+				startNode = current.left;
+			}else if(isLeftChild){
+				parent.left = current.left;
+			}else{
+				parent.right = current.left;
+			}
+		}
+		else if(current.left==null){
+			if(current==startNode){
+				startNode = current.right;
+			}else if(isLeftChild){
+				parent.left = current.right;
+			}else{
+				parent.right = current.right;
+			}
+		}else if(current.left!=null && current.right!=null){
+			
+			//now we have found the minimum element in the right sub tree
+			Node min = minNode(current.right);
+			current.data = min.data;
+			delete(current.right, current.data);
+		}		
+		return true;		
 	}
 
 	public void display(Node root) {
