9ecd26f0f41c5f0f37667918eb81e5476f3047fe
Ren, Yan <yan.ren@kronos.com>
2017-03-10 15:28:59 -0500

    some notes about hashing and hashMap

---
+++ test/src/main/java/hashing/HashCode.java
@@ -0,0 +1,25 @@
+package hashing;
+
+public class HashCode {
+	/*
+	 * hashCode() method is used to get a unique integer for given object. This
+	 * integer is used for determining the bucket location, when this object
+	 * needs to be stored in some HashTable like data structure. By default,
+	 * Object’s hashCode() method returns and integer representation of memory
+	 * address where object is stored.
+	 * 
+	 * equals() method, as name suggest, is used to simply verify the equality
+	 * of two objects. Default implementation simply check the object references
+	 * of two objects to verify their equality.
+	 * 
+	 * What is Hashing
+	 * 
+	 * Hashing in its simplest form, is a way to assigning a unique code for any
+	 * variable/object after applying any formula/algorithm on its properties. A
+	 * true Hashing function must follow this rule:
+	 * 
+	 * Hash function should return the same hash code each and every time, when
+	 * function is applied on same or equal objects. In other words, two equal
+	 * objects must produce same hash code consistently.
+	 */
+}
