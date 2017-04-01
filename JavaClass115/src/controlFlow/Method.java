2fd9a3fe506c18ac90acc6d8279af9b629355df1
Yan Ren <ryan.ren3@gmail.com>
2017-04-01 18:45:45 -0400

    method continue

--- JavaClass115/src/controlFlow/Method.java
+++ JavaClass115/src/controlFlow/Method.java
@@ -3,23 +3,29 @@ package controlFlow;
 public class Method {
 
 	public static void main(String[] args) {
-//		System.out.println("In the main");
-//		String content = "some text";
-//		printMethod(content);
-		int result = power(2, 3);
-		System.out.println(result);
+		double celsius = 10.0;
+		boolean cold = celsius > 10 ? false : true;
 	}
 
-	public static void printMethod(String input){
-		System.out.println("Inside print method");
-		System.out.println(input);
+	public static boolean cold(double celsius) {
+		return celsius > 10 ? false : true;
 	}
-	
-	public static int power(int a, int b){
-		int result = a;
-		for(int i=1; i < b; i ++){
-			result = a * result;
+
+	public static double convertToFahrenheit(double celsius) {
+		return celsius * 1.8 + 32.0;
+	}
+
+	public static void printString(String context) {
+		for (int i = 0; i < context.length(); i++) {
+			System.out.println(context.charAt(i));
 		}
-		return result;
+	}
+
+	public static void addString(String context) {
+		System.out.println("Original input: " + context);
+		System.out.println("Text after: ");
+		for (int i = 0; i < context.length(); i++) {
+			System.out.print((char) (context.charAt(i) + 1));
+		}
 	}
 }
